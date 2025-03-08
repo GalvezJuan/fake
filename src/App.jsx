b001306c5b0d4d5bcff22426a4df8502755fa41f
galvez <iancrux1@gmail.com>
2025-03-08 16:32:47 -0300

    3 commit. se agrega boton de compra y que se muestre la cantidad en carrito

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,36 @@ import './App.css'
 import { Products } from './Components/Products'
 
 function App() {
-  
-  const [products, setProducts]=useState([])
 
-  const getProducts =async()=>{
-    let response= await fetch("https://fakestoreapi.com/products")
-    let data=await response.json()
+  const [products, setProducts] = useState([])
+  const [shop, setShop] = useState([])
+
+  const getProducts = async () => {
+    let response = await fetch("https://fakestoreapi.com/products")
+    let data = await response.json()
     setProducts(data)
   }
 
-useEffect(()=>{
-  getProducts()
-},[])
+  useEffect(() => {
+    getProducts()
+  }, [])
 
-if (products.length == 0) {
-  return <>
-    <h1>Cargando...</h1>
-  </>
-}
+  const shopProducts = (product) => {
+    setShop([...shop, product])
+  }
+
+  if (products.length == 0) {
+    return <>
+      <h1>Cargando...</h1>
+    </>
+  }
 
 
   return (
     <>
-    <h1>Fake Store</h1>
-    <Products product={products}/>
+      <h1>Fake Store</h1>
+      <button disabled={shop.length == 0}>Productos en carrito: {shop.length} </button >
+        <Products product={products} shopProduct={shopProducts}/>
     </>
   )
 }
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,14 +1,15 @@
-export const Products = ({ product }) => {
+export const Products = ({ product, shopProduct }) => {
 
 
 
     return <div>
         {product.map((element, index) => {
 
-            return <div>
+            return <div key={index}>
                 <h4>{element.title}</h4>
                 <img src={element.image} height={200} width={300} alt="" />
                 <h5>{element.description}</h5>
+                <button onClick={()=>{shopProduct(element)}}>comprar</button>
             </div>
         })}
     </div>
